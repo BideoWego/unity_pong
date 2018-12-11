924c45af5a3fa423b37d989235b09af4b106973a
Bideo Wego <bideowego@gmail.com>
2018-12-10 22:43:35 -0500

    Initial commit

---
+++ Assets/Ball.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    public float speed;
+    public Rigidbody2D rb2d;
+
+	// Use this for initialization
+	void Start()
+    {
+        float rand = Random.Range(0, 2);
+        int direction = rand > 1 ? 1 : -1;
+        Vector2 v = new Vector2(direction * speed, 0);
+        rb2d = GetComponent<Rigidbody2D>();
+        rb2d.AddForce(v);
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate()
+    {
+		
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.CompareTag("Player"))
+        {
+            float x = rb2d.velocity.x;
+            float y = (rb2d.velocity.y / 2) +
+                (collision.collider.attachedRigidbody.velocity.y / 3);
+            rb2d.velocity = new Vector2(x, y);
+        }
+    }
+}
---
+++ Assets/Player.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public string playerType;
+    public float speed;
+    public Rigidbody2D rb2d;
+
+	// Use this for initialization
+	void Start()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate()
+    {
+        Vector2 pos = transform.position;
+
+        bool isPressed = Input.GetButton("Vertical " + playerType);
+        float direction = Input.GetAxisRaw("Vertical " + playerType);
+
+        if (isPressed)
+        {
+            pos.y += Time.deltaTime * direction * speed;
+        }
+
+        transform.position = new Vector2(pos.x, pos.y);
+    }
+}
